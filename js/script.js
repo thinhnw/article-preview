05a47b9b38d2179f9d9e9b5ce861f4d3664512ef
thinhnw <thinhnv0412@gmail.com>
2020-09-02 17:05:22 +0700

    Add share button and share media

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let shareButton = document.getElementById("share-button");
+let media = document.getElementsByClassName("media")[0];
+
+function mouseOverFunction() {
+
+    media.style.display = "block";
+}
+function mouseLeaveFunction() {
+    media.style.display = "none";
+}
