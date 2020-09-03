cb7c42e1765a53c8ab99e78a2ce35f7d2cfaa630
thinhnw <thinhnv0412@gmail.com>
2020-09-03 21:56:48 +0700

    Fix article footer

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let shareButton = document.getElementById("share-button");
+let shareButton = document.getElementsByClassName("share-btn")[0];
 let media = document.getElementsByClassName("media")[0];
 
 let status = false;
@@ -9,10 +9,12 @@ function mouseClickFunction() {
 function mouseCancel() {
     console.log(status);
     if (status) {
-        media.style.display = "grid";        
+        media.style.display = "grid";    
+        shareButton.classList.add("active");
         status = false;
         return;
     }
     media.style.display = "none";
+    shareButton.classList.remove("active");
     status = false;
 }
